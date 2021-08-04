ca7cbf1e46075895d3c142c66b715fc22509abc3
Viktor Nagy <126671+nagyv@users.noreply.github.com>
2021-08-04 23:50:05 +0200

    jQuery is not required

--- cropper_widget/static/js/cropper_widget/cropper_widget.js
+++ cropper_widget/static/js/cropper_widget/cropper_widget.js
@@ -1,31 +1,28 @@
-/**
- * Use cropper plugin on form's rendered with CropperWidget
- */
-jQuery(function ($) {
-    var croppers = $('img.cropper');
-    croppers.hide();
-    croppers.each(function (index, element) {
-        var cropper = $(element);
-        var input_name = cropper.data('cropper-for');
-        var $input = $('input[name=' + input_name + ']');
-        $input.on('change', function (changeEvent) {
-            var input = changeEvent.delegateTarget;
+window.addEventListener('DOMContentLoaded', function() {
+    const croppers = document.querySelectorAll('img.cropper')
+    croppers.forEach(function(cropperImg) {
+        const input_name = cropperImg.dataset.cropperFor
+        const cropperConf = cropperImg.dataset.cropperConf
+        const input = document.querySelector('input[name=' + input_name + ']')
+        const croppedInput = document.querySelector('input[name=' + input_name + '-cropped-data]')
+        const form = input.closest('form')
+        input.addEventListener('change', function(changeEv) {
+            const input = changeEv.target;
             if (input.files && input.files[0]) {
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    cropper.show();
-                    cropper.attr('src', e.target.result);
-                    var cropperOptions = cropper.data('cropper-conf');
-                    cropper.cropper(cropperOptions);
-                    cropper.cropper('replace', e.target.result);
-                    var form = $input.parents('form');
-                    var croppedInput = form.find('input[name=' + input_name + '-cropped-data]');
-                    form.submit(function (e) {
-                        croppedInput.val(cropper.cropper('getCroppedCanvas').toDataURL('image/jpeg'));
-                    });
-                };
-                reader.readAsDataURL(input.files[0]);
+                const file = input.files[0];
+                const type = file.type
+
+                // input.value = ''
+                cropperImg.src = URL.createObjectURL(file)
+
+                const cropper = new Cropper(cropperImg, cropperConf)
+                form.addEventListener('submit', function(submitEv) {
+
+                    croppedInput.value = cropper.getCroppedCanvas().toDataURL(type)
+                    // submitEv.preventDefault()
+
+                })
             }
-        });
-    });
+        })
+    })
 })
